ef5573c22921bdb7a687a39b11d51ed3ee1df515
hyperded <hyperdeath69@gmail.com>
2025-03-17 15:23:09 +0700

    OOP styled-ed

--- pac.py
+++ pac.py
@@ -1,69 +1,66 @@
 import pygame
 
+class visualization:
+    BLOCK_SIZE = 32
+    TITLE = 'Pac-Man'
 
-TITLE = 'Pac-Man'
-file="map.txt"
-
-# WORLD_SIZE = 28
-BLOCK_SIZE = 32
-
-
-assets = {
-                        '%': pygame.image.load('graphic_assets/wall.png'),
-                        'O': pygame.image.load('graphic_assets/power.png'),
-                        '.': pygame.image.load('graphic_assets/feast.png'),
-                        'P': pygame.image.load('graphic_assets/pac.png'),
-                                      }
-wallTextures = {
-     'horizontal': pygame.image.load('graphic_assets/wall.png'),
-     'vertical' : pygame.image.load('graphic_assets/wall2.png'),
-}
-map = []
-
-with open(file) as f:
-            for line in f:
-                row = []
-                for block in line:
-                    row.append(block)
-                map.append(row)
-
-
-WIDTH = len(map[0])*BLOCK_SIZE
-HEIGHT =len(map)*BLOCK_SIZE
-# dynamically assigning resolution
-pygame.init()
-pygame.display.set_caption(TITLE)
-screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
-# def getTexture(col, row):
-#     # for columns
-#     if col > 0 and col < len(map[row]) - 1 and map[row][col - 1] == '%' and map[row][col + 1] == '%':
-#         return wallTextures['vertical']
-#     if row > 0 and row < len(map) - 1 and map[row - 1][col] == '%' and map[row + 1][col] == '%':
-#         return wallTextures['horizontal']
-         
-def draw():
+    
+    def __init__(self, file='map.txt'):
+        self.file="map.txt"
+        self.map = []
+        with open(file) as f:
+                    for line in f:
+                        row = []
+                        for block in line:
+                            row.append(block)
+                        self.map.append(row)
+        WIDTH = len(self.map[0])*self.BLOCK_SIZE
+        HEIGHT =len(self.map)*self.BLOCK_SIZE
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
+    def draw(self):
+        assets = {
+            '%': pygame.image.load('graphic_assets/wall.png'),
+            'O': pygame.image.load('graphic_assets/power.png'),
+            '.': pygame.image.load('graphic_assets/feast.png'),
+            'P': pygame.image.load('graphic_assets/pac.png'),}
         
-        for y, row in enumerate(map):
+        for y, row in enumerate(self.map):
             for x, block in enumerate(row):
                 image = assets.get(block, None)
-                # texture = getTexture(x, y)
                 if image:
-                    # scale
-                    # scaledTextures = pygame.transform.scale(assets[block], (BLOCK_SIZE, BLOCK_SIZE))
-                    # blitzkrieg
-                    screen.blit(assets[block], (x*BLOCK_SIZE, y*BLOCK_SIZE))
-                # if texture:
-                #     # scaledTextures = pygame.transform.scale(texture, (BLOCK_SIZE, BLOCK_SIZE))
-                #     screen.blit(texture, (x*BLOCK_SIZE, y*BLOCK_SIZE))
+                    self.screen.blit(assets[block], (x*self.BLOCK_SIZE, y*self.BLOCK_SIZE))
 
 
 
-playing = True
-while playing:
-    screen.fill((0,0,0))
-    draw()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            playing = False
-    pygame.display.flip()
-    
+    def play(self):
+        
+        pygame.init()
+        pygame.display.set_caption(self.TITLE)
+        playing = True
+        while playing:
+            self.screen.fill((0,0,0))
+            self.draw()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    playing = False
+            pygame.display.flip()
+            
+
+
+# WORLD_SIZE = 28
+
+
+kaizen = visualization()
+kaizen.play()
+# wallTextures = {
+#      'horizontal': pygame.image.load('graphic_assets/wall.png'),
+#      'vertical' : pygame.image.load('graphic_assets/wall2.png'),
+# }
+
+
+
+# dynamically assigning resolution
+
+
+# 
+
